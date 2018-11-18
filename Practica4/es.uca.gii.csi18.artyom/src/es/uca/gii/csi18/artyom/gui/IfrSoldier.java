2714c015945f94c34e10b8a2ee539c8d929d6210
Kevin López <33726914+Lokeon@users.noreply.github.com>
2018-11-18 01:17:23 +0100

    Add files via upload

--- Practica4/es.uca.gii.csi18.artyom/src/es/uca/gii/csi18/artyom/gui/IfrSoldier.java
+++ Practica4/es.uca.gii.csi18.artyom/src/es/uca/gii/csi18/artyom/gui/IfrSoldier.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class IfrSoldier extends JInternalFrame {
+	private static final long serialVersionUID = 1L;
 	private JTextField txtNombre;
 	private JTextField txtHorasDeGuardia;
 	private Soldado _soldier = null;
